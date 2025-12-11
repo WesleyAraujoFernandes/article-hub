b84e3860a0192c603a68c64bb1dd5ba517c618f2
WesleyAraujoFernandes <wesley@viaapia.com.br>
2025-12-11 00:08:59 -0300

    UserInfoRepository created

--- src/main/java/com/article/hub/entity/UserInfo.java
+++ src/main/java/com/article/hub/entity/UserInfo.java
@@ -3,6 +3,9 @@ package com.article.hub.entity;
 import java.io.Serializable;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,5 +18,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserInfo implements Serializable {
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String isDeletable;
+    private String status;
+
+    public UserInfo(int id, String name, String email, String status) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.status = status;
+    }
 }
